c3059d7454566f68fa9fa11c47fabc4fbd68b7db
Anastasia Abakumova <anastasia.abakumovaa@gmail.com>
2024-09-18 22:27:56 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+const noBtn = document.querySelector('.no__btn');
+const responseContainer = document.querySelector('.response-container');
+
+noBtn.addEventListener('mouseover', () => {
+noBtn.style.animation = 'move 0.5s infinite alternate';
+});
+
+noBtn.addEventListener('mouseleave', () => {
+noBtn.style.animation = 'none';
+});
+
+function showResponse() {
+   responseContainer.style.display = 'block';
+   setTimeout(() => {
+       responseContainer.style.display = 'none'; 
+   }, 2000);
+}
+
+noBtn.addEventListener('click', () => {
+   showResponse(); 
+});
+
+const agreeBtn = document.querySelector('.agree__btn');
+   const container = document.querySelector('.btn__container');
+   function moveButton() {
+       const containerWidth = container.clientWidth;
+       const containerHeight = container.clientHeight;
+
+       const btnWidth = agreeBtn.offsetWidth;
+       const btnHeight = agreeBtn.offsetHeight;
+
+       const randomX = Math.random() * (containerWidth - btnWidth);
+       const randomY = Math.random() * (containerHeight - btnHeight);
+
+       agreeBtn.style.left = `${randomX}px`;
+       agreeBtn.style.top = `${randomY}px`;
+   }
+
+   agreeBtn.addEventListener('mouseover', moveButton);
